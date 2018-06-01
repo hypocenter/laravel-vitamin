05769971cdf093b6fcca2c261af360d084ed1e8a
hypo <hypomail@gmail.com>
2018-06-01 17:49:25 +0800

    update comment

--- src/Transformer/EquivalentTransformer.php
+++ src/Transformer/EquivalentTransformer.php
@@ -4,6 +4,6 @@ namespace Hypocenter\LaravelVitamin\Transformer;
 
 /**
  * Class EquivalentTransformer
- * @deprecated use DefaultTransformer instead
+ * @deprecated use DefaultTransformer instead, will remove in v0.2.0
  */
 class EquivalentTransformer extends DefaultTransformer {}
