032ade4921f0a86f4a8e9fdc0821e0e628104927
hypo <hypomail@gmail.com>
2018-06-03 23:40:11 +0800

    refactor

--- src/Repository/Contracts/Criteria.php
+++ src/Repository/Contracts/Criteria.php
@@ -5,5 +5,13 @@ namespace Hypocenter\LaravelVitamin\Repository\Contracts;
 
 interface Criteria
 {
+    const OP_EQ   = 'eq';
+    const OP_LIKE = 'like';
+    const OP_GT   = 'gt';
+    const OP_GTE  = 'gte';
+    const OP_LT   = 'lt';
+    const OP_LTE  = 'lte';
+    const OP_IN   = 'in';
+    
     public function apply($builder);
 }
