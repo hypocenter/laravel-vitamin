14eb475c029d45d190e8cf43e25b98df874ea76c
hypo <hypomail@gmail.com>
2018-06-04 08:28:58 +0800

    change: remove deprecated code

--- src/Repository/Criteria/RequestCriteria.php
+++ src/Repository/Criteria/RequestCriteria.php
@@ -18,29 +18,6 @@ class RequestCriteria extends AbstractCriteria
         $this->parser = $parser;
     }
 
-    /**
-     * @param null $searchable
-     * @param null $field
-     *
-     * @return RequestCriteria|static
-     *
-     * @deprecated use set() instead, will remove in v0.2.0
-     */
-    public function create($searchable = null, $field = null)
-    {
-        $c = resolve(get_called_class());
-
-        if ($searchable) {
-            $c->setSearchable($searchable);
-        }
-
-        if ($field) {
-            $c->setField($field);
-        }
-
-        return $c;
-    }
-
     public function set($searchable = null, $field = null)
     {
         if ($searchable) {
--- src/Transformer/EquivalentTransformer.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Hypocenter\LaravelVitamin\Transformer;
-
-/**
- * Class EquivalentTransformer
- * @deprecated use DefaultTransformer instead, will remove in v0.2.0
- */
-class EquivalentTransformer extends DefaultTransformer {}
